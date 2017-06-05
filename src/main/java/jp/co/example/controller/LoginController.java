b51c87c6965ddf75ec48b3f664ec40662c8f7bcc
takahashi <2017se_r.takahashi@axiz.co.jp>
2017-06-05 11:43:34 +0900

    no message

--- src/main/java/jp/co/example/controller/LoginController.java
+++ src/main/java/jp/co/example/controller/LoginController.java
@@ -4,12 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import enums.JspPage;
+import enums.LogEnum;
+import lombok.extern.slf4j.Slf4j;
+import util.Util;
 
+//login.jspへの遷移を記述
+@Slf4j
 @Controller
 public class LoginController {
 
+	//login.jspからの遷移
 	@RequestMapping("/login")
 	public String getLogin() {
+		log.info(Util.getMethodName() + LogEnum.START.getLogValue());
+
+		log.info(Util.getMethodName() + LogEnum.END.getLogValue());
 		return JspPage.LOGIN.getPageName();
 	}
 
