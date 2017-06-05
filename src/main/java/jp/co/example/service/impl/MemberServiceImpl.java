ceb86260f11e22be119e90c2d661977f098c57bb
mkt-S <fr0m.ma.t0.s@gmail.com>
2017-06-05 16:15:47 +0900

    no message

---
+++ src/main/java/jp/co/example/service/impl/MemberServiceImpl.java
@@ -0,0 +1,18 @@
+package jp.co.example.service.impl;
+
+import java.util.List;
+
+import jp.co.example.dao.UsersDao;
+import jp.co.example.entity.Users;
+import jp.co.example.service.MemberService;
+
+public class MemberServiceImpl implements MemberService{
+
+	UsersDao UsersDao;
+
+	public List<Users> Member(int roomId, int comId){
+		//return UserDao.FindMember(roomId,comId);←ここから
+		return null;
+	}
+
+}
