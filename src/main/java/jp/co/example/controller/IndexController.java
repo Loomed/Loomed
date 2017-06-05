67872bbf926bb70a234f10e3fe9875edbba87a58
mkt-S <fr0m.ma.t0.s@gmail.com>
2017-06-05 10:40:53 +0900

    index遷移_0605

--- src/main/java/jp/co/example/controller/IndexController.java
+++ src/main/java/jp/co/example/controller/IndexController.java
@@ -1,17 +1,59 @@
 package jp.co.example.controller;
 
-import javax.validation.Valid;
+import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import enums.LogEnum;
+import jp.co.example.entity.Users;
+import jp.co.example.service.Generalized;
+import jp.co.example.service.IndexService;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import util.Util;
 
+@Slf4j
+@Controller
 @Getter
 public class IndexController {
-	@RequestMapping(value = "/index")
-	public String postLogin(@Valid LoginForm form, BindingResult result, Model model){
+	@Autowired
+	IndexService IndexService;
+	HttpServletRequest request;
+
+	@RequestMapping("/index")
+	public String getIndex(@RequestParam("id") String id, @RequestParam("pass") String pass,
+			HttpServletRequest request) {
+		log.info(Util.getMethodName() + LogEnum.START.getLogValue());
+
+		int LoginId = 0;
+		String LoginPass = "";
+
+		// 入力されたidがintかどうか判定
+		log.info(LogEnum.IF.getLogValue() + "Generalized.isNum(id)");
+		if (Generalized.isNum(id)) {
+			log.info(LogEnum.IF.getLogValue() + "(Generalized.isNum(id) == true)");
+			LoginId = Integer.parseInt(id);
+		} else {
+			log.info(LogEnum.FALSE.getLogValue());
+		}
+
+		// 入力されたpassが0文字以上か判定
+		log.info(LogEnum.IF.getLogValue() + "Generalized.isNum(id)");
+		if (pass.length() > 0) {
+			log.info(LogEnum.IF.getLogValue() + "(pass.length() > 0 == true)");
+			LoginPass = pass;
+		} else {
+			log.info(LogEnum.FALSE.getLogValue());
+		}
+
+		// ログイン判定
+		Users LoginUser = IndexService.LoginJudge(LoginId, LoginPass);
+
+		log.info(Util.getMethodName() + LogEnum.END.getLogValue());
+		return null;
 
 	}
 
---
+++ src/main/java/jp/co/example/service/impl/IndexServiceImpl.java
@@ -0,0 +1,17 @@
+package jp.co.example.service.impl;
+
+import jp.co.example.dao.UsersDao;
+import jp.co.example.entity.Users;
+import jp.co.example.service.IndexService;
+
+public class IndexServiceImpl implements IndexService{
+
+	UsersDao UsersDao;
+
+	public Users LoginJudge(int id, String pass){
+
+
+
+		return null;
+	}
+}
