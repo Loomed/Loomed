b958f2962054981e6ee5cf27acdb2444975351c2
sakata kenichi <2017se_sakata@axiz.co.jp>
2017-06-05 11:45:28 +0900

    user

--- src/main/java/jp/co/example/controller/UserChangeController.java
+++ src/main/java/jp/co/example/controller/UserChangeController.java
@@ -4,18 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import enums.JspPage;
+import enums.LogEnum;
+import lombok.extern.slf4j.Slf4j;
+import util.Util;
 
+@Slf4j
 @Controller
 public class UserChangeController {
 
 	/**
-	* userinfoへの遷移
+	* userchangeへの遷移
 	* 2017/06/05
 	* @Author sakata
 	*/
-	@RequestMapping("/userinfo")
-	public String getUserInfo() {
-		return JspPage.USERINFO.getPageName();
+	@RequestMapping("/userchange")
+	public String getUserChange() {
+		log.info(Util.getMethodName() + LogEnum.START.getLogValue());
+
+		return JspPage.USERCHANGE.getPageName();
 	}
 
 }
--- src/main/java/jp/co/example/controller/UserInfoController.java
+++ src/main/java/jp/co/example/controller/UserInfoController.java
@@ -1,21 +1,44 @@
 package jp.co.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import enums.JspPage;
+import enums.LogEnum;
+import jp.co.example.entity.Users;
+import lombok.extern.slf4j.Slf4j;
+import util.Util;
 
+@Slf4j
 @Controller
 public class UserInfoController {
+
+	@Autowired
+	private
 
 	/**
 	* userchangeへの遷移
 	* 2017/06/05
 	* @Author sakata
 	*/
-	@RequestMapping("/userchange")
-	public String getUserInfo() {
-		return JspPage.USERCHANGE.getPageName();
+	@RequestMapping("/userinfo")
+	public String getUserInfo() { //セッションからユーザの値を取得する
+		log.info(Util.getMethodName() + LogEnum.START.getLogValue());
+
+		Users user = new Users();
+		user.setUserId(1);
+
+
+
+		return JspPage.USERINFO.getPageName();
 	}
+
+//	@RequestMapping(value="/userinfo", method=RequestMethod.POST)
+//	public String postUserInfo() {
+//		log.info(Util.getMethodName() + LogEnum.START.getLogValue());
+//
+//		return JspPage.USERINFO.getPageName();
+//	}
 
 }
