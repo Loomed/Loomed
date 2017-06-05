c211481a01eb3097dfdd18395ae0c57b08be7c64
TakumiKajiwara <kaamjai82@gmail.com>
2017-06-05 09:28:28 +0900

    6/5 9:27、MailController

--- src/main/java/jp/co/example/controller/MailController.java
+++ src/main/java/jp/co/example/controller/MailController.java
@@ -4,14 +4,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.Getter;
 
-//home、roothomeからの遷移を記述
+//home、roothomeからのmail.jspの遷移を記述
 @Getter
 public class MailController {
 
-	//home.jspのmailから遷移
+	//home.jsp、roothome.jspのmailから遷移
 	@RequestMapping(value = "/mail")
-	public String postLogin() {
-			return "mail";
+	public String getMail() {
+		return "mail";
+
 		}
 
 }
