07d2c1c586bc8779e4b99e7b6b60ece2064dda6f
TakumiKajiwara <kaamjai82@gmail.com>
2017-06-05 10:15:54 +0900

    no message

--- src/main/java/jp/co/example/controller/MailController.java
+++ src/main/java/jp/co/example/controller/MailController.java
@@ -2,16 +2,27 @@ package jp.co.example.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import enums.JspPage;
+import enums.LogEnum;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import util.Util;
 
 //home、roothomeからのmail.jspの遷移を記述
 @Getter
+@Slf4j
+
 public class MailController {
 
 	//home.jsp、roothome.jspのmailから遷移
 	@RequestMapping(value = "/mail")
 	public String getMail() {
-		return "mail";
+		log.info(Util.getMethodName() + LogEnum.START.getLogValue());
+
+		//メール処理はサービスに投げる
+
+		log.info(Util.getMethodName() + LogEnum.END.getLogValue());
+		return JspPage.MAIL.getPageName();
 
 		}
 
