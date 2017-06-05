9d139017574ec9c9d3c3e9b736c9e659dcb27c9c
TakumiKajiwara <kaamjai82@gmail.com>
2017-06-05 12:14:53 +0900

    0605 1213、MailsDaoImpl一時コメント化

--- src/main/java/jp/co/example/dao/impl/MailsDaoImpl.java
+++ src/main/java/jp/co/example/dao/impl/MailsDaoImpl.java
@@ -1,14 +1,13 @@
-package jp.co.example.dao.impl;
-
-import org.springframework.stereotype.Repository;
-
-import jp.co.example.dao.MailsDao;
-import jp.co.example.entity.Mails;
-
-@Repository
-public class MailsDaoImpl implements MailsDao {
-	@Override
-	Mails findByIdAndTitleAndContents(Integer mailID, String mailTitle, String mailContents) {
-
-	}
-}
+//package jp.co.example.dao.impl;
+//
+//import org.springframework.stereotype.Repository;
+//
+//import jp.co.example.dao.MailsDao;
+//
+//@Repository
+//public class MailsDaoImpl implements MailsDao {
+//	@Override
+//	Mails findByIdAndTitleAndContents(Integer mailID, String mailTitle, String mailContents) {
+//
+//	}
+//}
