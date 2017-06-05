5da2c488543d7b5933aacdf65255786dd1bf9bc1
yukihiro yoshida <2017se_yoshida@axiz.co.jp>
2017-06-05 18:59:40 +0900

    commit

--- src/main/java/jp/co/example/service/impl/MemberServiceImpl.java
+++ src/main/java/jp/co/example/service/impl/MemberServiceImpl.java
@@ -2,10 +2,13 @@ package jp.co.example.service.impl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import jp.co.example.dao.UsersDao;
 import jp.co.example.entity.Users;
 import jp.co.example.service.MemberService;
 
+@Service
 public class MemberServiceImpl implements MemberService {
 
 	UsersDao UsersDao;
