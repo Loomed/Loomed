966e17f1276df8e9e678b613bf788bc8f518399a
sakata kenichi <2017se_sakata@axiz.co.jp>
2017-06-05 11:53:10 +0900

    user

--- src/main/java/jp/co/example/controller/UserChangeController.java
+++ src/main/java/jp/co/example/controller/UserChangeController.java
@@ -4,15 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import enums.JspPage;
+<<<<<<< HEAD
 import enums.LogEnum;
 import lombok.extern.slf4j.Slf4j;
 import util.Util;
 
 @Slf4j
+=======
+
+>>>>>>> 453dcaf76f6b623ea717da9ae8658f0f08ca8088
 @Controller
 public class UserChangeController {
 
 	/**
+<<<<<<< HEAD
 	* userchangeへの遷移
 	* 2017/06/05
 	* @Author sakata
@@ -22,6 +27,15 @@ public class UserChangeController {
 		log.info(Util.getMethodName() + LogEnum.START.getLogValue());
 
 		return JspPage.USERCHANGE.getPageName();
+=======
+	* userinfoへの遷移
+	* 2017/06/05
+	* @Author sakata
+	*/
+	@RequestMapping("/userinfo")
+	public String getUserInfo() {
+		return JspPage.USERINFO.getPageName();
+>>>>>>> 453dcaf76f6b623ea717da9ae8658f0f08ca8088
 	}
 
 }
--- src/main/java/jp/co/example/controller/UserInfoController.java
+++ src/main/java/jp/co/example/controller/UserInfoController.java
@@ -1,10 +1,14 @@
 package jp.co.example.controller;
 
+<<<<<<< HEAD
 import org.springframework.beans.factory.annotation.Autowired;
+=======
+>>>>>>> 453dcaf76f6b623ea717da9ae8658f0f08ca8088
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import enums.JspPage;
+<<<<<<< HEAD
 import enums.LogEnum;
 import jp.co.example.entity.Users;
 import lombok.extern.slf4j.Slf4j;
@@ -17,11 +21,18 @@ public class UserInfoController {
 	@Autowired
 	private
 
+=======
+
+@Controller
+public class UserInfoController {
+
+>>>>>>> 453dcaf76f6b623ea717da9ae8658f0f08ca8088
 	/**
 	* userchangeへの遷移
 	* 2017/06/05
 	* @Author sakata
 	*/
+<<<<<<< HEAD
 	@RequestMapping("/userinfo")
 	public String getUserInfo() { //セッションからユーザの値を取得する
 		log.info(Util.getMethodName() + LogEnum.START.getLogValue());
@@ -41,4 +52,11 @@ public class UserInfoController {
 //		return JspPage.USERINFO.getPageName();
 //	}
 
+=======
+	@RequestMapping("/userchange")
+	public String getUserInfo() {
+		return JspPage.USERCHANGE.getPageName();
+	}
+
+>>>>>>> 453dcaf76f6b623ea717da9ae8658f0f08ca8088
 }
