bf2cdd8aa9a5133c0b6cd9bbb18cea8e0046ea2a
takahashi <2017se_r.takahashi@axiz.co.jp>
2017-06-05 14:07:52 +0900

    no message

--- src/main/java/jp/co/example/controller/LoginController.java
+++ src/main/java/jp/co/example/controller/LoginController.java
@@ -1,10 +1,18 @@
 package jp.co.example.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import enums.JspPage;
 import enums.LogEnum;
+import jp.co.example.entity.Users;
+import jp.co.example.service.LoginService;
 import lombok.extern.slf4j.Slf4j;
 import util.Util;
 
@@ -13,7 +21,10 @@ import util.Util;
 @Controller
 public class LoginController {
 
-	//login.jspからの遷移
+	@Autowired
+	private LoginService loginService;
+
+	// login.jspからの遷移
 	@RequestMapping("/login")
 	public String getLogin() {
 		log.info(Util.getMethodName() + LogEnum.START.getLogValue());
@@ -22,4 +33,17 @@ public class LoginController {
 		return JspPage.LOGIN.getPageName();
 	}
 
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String postLogin(@Valid LoginForm form, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return "login";
+		}
+
+		Users users = loginService.findByIdAndPass(form.getId(), form.getPass());
+
+		if (users != null) {
+			model.addAttribute(users);
+			return "login";
+		}
+	}
 }
