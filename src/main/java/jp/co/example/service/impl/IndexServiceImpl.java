b311ace7be04448c01902686a21ebb5ea75f81b4
admin-HP\axiz <2017se_yamane@axiz.co.jp>
2017-06-05 10:55:44 +0900

    no message

--- src/main/java/jp/co/example/service/impl/IndexServiceImpl.java
+++ src/main/java/jp/co/example/service/impl/IndexServiceImpl.java
@@ -1,9 +1,12 @@
 package jp.co.example.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import jp.co.example.dao.UsersDao;
 import jp.co.example.entity.Users;
 import jp.co.example.service.IndexService;
 
+@Service
 public class IndexServiceImpl implements IndexService{
 
 	UsersDao UsersDao;
