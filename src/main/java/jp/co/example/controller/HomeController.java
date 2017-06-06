5f3b1a6d3a9992cdd5c58ac6e839d581d9522a82
レイさん <2017se_yamane@axiz.co.jp>
2017-06-06 14:56:45 +0900

    ホーム関連完成（仮）

--- src/main/java/jp/co/example/controller/HomeController.java
+++ src/main/java/jp/co/example/controller/HomeController.java
@@ -31,46 +31,62 @@ public class HomeController {
 
 		log.info(Util.getMethodName() + LogEnum.START.getLogValue());
 
-		//セッションの受け取りと必要なデータ生成
-		//Users user = (Users) session.getAttribute(ScopeKey.LOGINUSER.getScopeKey());
+		// セッションの受け取りと必要なデータ生成
+		// Users user = (Users)session.getAttribute(ScopeKey.LOGINUSER.getScopeKey());
 		Trainings tr = new Trainings();
 		String cnt = null;
-		Integer maps =0;
+		String maps = null;
+		Integer tid = 0;
+		String url = null;
+		int index = 0;
+		String id = null;
+		int training_id = 0;
+		try {
+			url = reques.getRequestURI();
+			index = url.lastIndexOf("=");
+			id = url.substring(index);
+
+			if (id != null) {
+				training_id = Integer.parseInt(id);
+			}
+			tr = HS.getTrainingName(training_id);
+			session.setAttribute("tr", tr);
+		} catch (Exception e) {
+
+		}
 		List<Schedules> list = new ArrayList<Schedules>();
 		// テストデータ
 		Users user = new Users(10, "test", "稲田 泳助", 2, 3);
-		//サービスへ
+		// サービスへ
 		System.out.println(user);
 		maps = HS.getTrainingid(user);
 		cnt = HS.getNewMails(user);
 		list = HS.getInpoSche();
-		//tr = HS.getTrainingName();
-
-		if(cnt.equals("0")){
-			cnt ="新着無し";
+		tr = HS.getTrainingName(training_id);
+		if (cnt.equals("0")) {
+			cnt = "新着無し";
 		}
 		// ボタンで遷移先を決定
 		/*
 		 * if ("userinfo".equals(button)) { return
-		JspPage.USERINFO.getPageName();
-		  } else if ("member".equa
+		 * JspPage.USERINFO.getPageName(); } else if ("member".equa
 		 * ("shareconfig".equals(button)) { return
-		 * JspPage.SHARECONFIG.getPagls(button)) {
-		 * return JspPage.MEMBER.getPageName(); } else ifeName(); }
+		 * JspPage.SHARECONFIG.getPagls(button)) { return
+		 * JspPage.MEMBER.getPageName(); } else ifeName(); }
 		 */
-		session.setAttribute("tr", tr);
+
 		session.setAttribute("user", user);
-		model.addAttribute("list",list);
-		model.addAttribute("cnt",cnt);
+		model.addAttribute("list", list);
+		model.addAttribute("cnt", cnt);
 		log.info(Util.getMethodName() + LogEnum.END.getLogValue());
 		return JspPage.HOME.getPageName();
 	}
 
-	/*@RequestMapping("/home")
-	public String getHome() {
-		log.info(Util.getMethodName() + LogEnum.START.getLogValue());
-		log.info(Util.getMethodName() + LogEnum.END.getLogValue());
-		return JspPage.HOME.getPageName();
-	}*/
+	/*
+	 * @RequestMapping("/home") public String getHome() {
+	 * log.info(Util.getMethodName() + LogEnum.START.getLogValue());
+	 * log.info(Util.getMethodName() + LogEnum.END.getLogValue()); return
+	 * JspPage.HOME.getPageName(); }
+	 */
 
 }
