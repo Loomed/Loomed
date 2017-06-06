b6f211e6b00da0e4504b6bba2f69ff4b99098180
mkt-S <fr0m.ma.t0.s@gmail.com>
2017-06-06 09:41:05 +0900

    Member一式作成過程+index一部修正中

--- src/main/java/jp/co/example/service/impl/MemberServiceImpl.java
+++ src/main/java/jp/co/example/service/impl/MemberServiceImpl.java
@@ -1,21 +1,40 @@
 package jp.co.example.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import enums.LogEnum;
 import jp.co.example.dao.UsersDao;
 import jp.co.example.entity.Users;
 import jp.co.example.service.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import util.Util;
 
+@Slf4j
 @Service
 public class MemberServiceImpl implements MemberService {
 
 	UsersDao UsersDao;
 
-	public List<Users> Member(int roomId, int comId) {
-		// return UserDao.FindMember(roomId,comId);←ここから
-		return null;
+	public List<Users> Member(int myRoomId, int comId, int nowRoomId) {
+		log.info(Util.getMethodName() + LogEnum.START.getLogValue());
+
+		//←ここから
+		List<Users> members = new ArrayList<>();
+
+		log.info(LogEnum.IF.getLogValue() + "myRoomId == nowRoomId");
+		if(myRoomId == nowRoomId){
+			log.info(LogEnum.TRUE.getLogValue() + "FindRoomMemberから自身と同じ教室メンバーを受け取る");
+			//members = UsersDao.FindRoomMember(myRoomId);
+		}else{
+			log.info(LogEnum.FALSE.getLogValue() + "FindCompMemberから自身と同じ企業メンバーを受け取る");
+			//members = UsersDao.FindCompMember(comId);
+		}
+
+		log.info(Util.getMethodName() + LogEnum.END.getLogValue());
+		return members;
 	}
 
 }
