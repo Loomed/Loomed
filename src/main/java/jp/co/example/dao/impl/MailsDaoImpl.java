518ee71c96a893d6550466b630c54167bf06ed5a
レイさん <2017se_yamane@axiz.co.jp>
2017-06-06 09:17:04 +0900

    マップとスケジュールDAO

--- src/main/java/jp/co/example/dao/impl/MailsDaoImpl.java
+++ src/main/java/jp/co/example/dao/impl/MailsDaoImpl.java
@@ -4,13 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import enums.LogEnum;
 import jp.co.example.dao.MailsDao;
 import jp.co.example.entity.Mails;
 import jp.co.example.entity.Users;
+import lombok.extern.slf4j.Slf4j;
+import util.Util;
+
 
 @Repository
+@Slf4j
 public class MailsDaoImpl implements MailsDao {
-	private static final String SQL_SELECT_MAILS_WHERE_OPENFLAG = "SELECT count(open_flag) FROM mails WHERE open_flag=true;";
+	private static final String SQL_SELECT_MAILS_WHERE_OPENFLAG = "SELECT count(open_flag) FROM mails WHERE open_flag=true AND reception_user_id = ?;";
 
 	@Autowired
 	JdbcTemplate jt;
@@ -22,7 +27,10 @@ public class MailsDaoImpl implements MailsDao {
 
 	@Override
 	public int getNewMails(Users user) {
+		log.info(Util.getMethodName() + LogEnum.START.getLogValue());
 		int cnt =0;
+		cnt = jt.queryForObject("SQL_SELECT_MAILS_WHERE_OPENFLAG", Integer.class);
+		log.info(Util.getMethodName() + LogEnum.END.getLogValue());
 		return cnt;
 	}
 }
