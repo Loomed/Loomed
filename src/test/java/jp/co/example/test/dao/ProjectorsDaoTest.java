3ff39a02b3f3f5d08d6c205c174d566aa3a1fb47
sakata kenichi <2017se_sakata@axiz.co.jp>
2017-06-08 15:49:34 +0900

    no message

--- src/test/java/jp/co/example/test/dao/ProjectorsDaoTest.java
+++ src/test/java/jp/co/example/test/dao/ProjectorsDaoTest.java
@@ -6,27 +6,130 @@ import org.junit.*;
 import org.junit.runner.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.test.context.*;
+import org.springframework.test.context.*;
 import org.springframework.test.context.junit4.*;
+import org.springframework.transaction.annotation.*;
 
+import jp.co.example.*;
 import jp.co.example.dao.*;
+import jp.co.example.entity.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
+@ContextConfiguration(classes = LoomedApplication.class)
 public class ProjectorsDaoTest {
-//	List<Projectors> selectProjectorsWhereTrainingIdAndDate(Integer trainingId, Timestamp dateMin, Timestamp dateMax);
-//	List<Projectors> selectProjectorsWhereTrainingIdAndDateTime(Integer trainingId, Timestamp dateTime);
-
 
 	@Autowired
 	private ProjectorsDao projectorsDao;
 
 	@Test
-	public void メールが取得できる() throws Exception{
-		assertTrue(true);
+	public void テスト34() throws Exception {
+
+		// テストデータ作成
+		Projectors pro = projectorsDao.selectProjectorsWhereTrainingIdAndDate(trainingId, dateMin, dateMax);
+
+		assertTrue(false);
 	}
 
 	@Test
-	public void メールが取得できない() throws Exception{
-		assertTrue(true);
+	public void テスト35() throws Exception {
+
+		// テストデータ作成
+		Projectors pro = projectorsDao.selectProjectorsWhereTrainingIdAndDate(trainingId, dateMin, dateMax);
+
+		assertTrue(false);
 	}
+
+	@Test
+	public void テスト36() throws Exception {
+
+		// テストデータ作成
+		Projectors pro = projectorsDao.selectProjectorsWhereTrainingIdAndDateTime(trainingId, dateMin, dateMax);
+
+		assertTrue(false);
+	}
+
+	@Test
+	public void テスト37() throws Exception {
+
+		// テストデータ作成
+		Projectors pro = projectorsDao.selectProjectorsWhereTrainingIdAndDateTime(trainingId, dateMin, dateMax);
+
+		assertTrue(false);
+	}
+
+	@Test
+	public void テスト38() throws Exception {
+
+		// テストデータ作成
+		int cnt = projectorsDao.deleteWhereProjectorId(projectorId)
+
+		assertTrue(false);
+	}
+
+	@Test
+	public void テスト39() throws Exception {
+
+		// テストデータ作成
+		int cnt = projectorsDao.deleteWhereProjectorId(projectorId)
+
+		assertTrue(false);
+	}
+
+	@Test
+	public void テスト40() throws Exception {
+
+		// テストデータ作成
+		int cnt = projectorsDao.insertProjectoReserve(trainingId, projectorNumber, userId, reserveTime))
+
+		assertTrue(false);
+	}
+
+	@Test
+	public void テスト41() throws Exception {
+
+		// テストデータ作成
+		int cnt = projectorsDao.insertProjectoReserve(null, null, null, null);
+
+		assertTrue(false);
+	}
+
+	@Test
+	public void テスト42() throws Exception {
+
+		// テストデータ作成
+		int cnt = projectorsDao.insertProjectoReserve(null, null, null, null);
+
+		assertTrue(false);
+	}
+
+	@Test
+	public void テスト43() throws Exception {
+
+		// テストデータ作成
+		int cnt = projectorsDao.insertProjectoReserve(null, null, null, null);
+
+		assertTrue(false);
+	}
+
+	@Test
+	public void テスト44() throws Exception {
+
+		// テストデータ作成
+		int cnt = projectorsDao.insertProjectoReserve(null, null, null, null);
+
+		assertTrue(false);
+	}
+
+	@Test
+	public void テスト45() throws Exception {
+
+		// テストデータ作成
+		int cnt = projectorsDao.insertProjectoReserve(null, null, null, null);
+
+		assertTrue(false);
+	}
+
+
 }
--- src/test/java/jp/co/example/test/dao/SchedulesDaoTest.java
+++ src/test/java/jp/co/example/test/dao/SchedulesDaoTest.java
@@ -2,7 +2,9 @@ package jp.co.example.test.dao;
 
 import static org.junit.Assert.*;
 
+import java.sql.*;
 import java.util.*;
+import java.util.Date;
 
 import org.junit.*;
 import org.junit.runner.*;
@@ -66,7 +68,115 @@ public class SchedulesDaoTest {
 		// テストデータ作成
 		int cnt = scheduleDao.updateScheduleWhereScheduleId();
 
-		assertEquals(expected, actual);
+		assertTrue(false);
+	}
+
+	@Test
+	public void テスト50() throws Exception {
+
+		// テストデータ作成
+		int cnt = scheduleDao.updateScheduleWhereScheduleId();
+
+		assertTrue(false);
+	}
+
+	@Test
+	public void テスト51() throws Exception {
+
+		// テストデータ作成
+		int cnt = scheduleDao.updateScheduleWhereScheduleId();
+
+		assertTrue(false);
+	}
+
+	@Test
+	public void テスト52() throws Exception {
+
+		// テストデータ作成
+		int cnt = scheduleDao.updateScheduleWhereScheduleId();
+
+		assertTrue(false);
+	}
+
+	@Test
+	public void テスト53() throws Exception {
+
+		// テストデータ作成
+		int cnt = scheduleDao.updateScheduleWhereScheduleId();
+
+		assertTrue(false);
+	}
+
+	@Test
+	public void テスト54() throws Exception {
+
+		// テストデータ作成
+		int cnt = scheduleDao.deleteScheduleWhereScheduleId();
+
+		assertTrue(false);
+	}
+
+	@Test
+	public void テスト55() throws Exception {
+
+		// テストデータ作成
+		int cnt = scheduleDao.deleteScheduleWhereScheduleId();
+
+		assertTrue(false);
+	}
+
+	@Test
+	public void テスト56() throws Exception {
+
+		// テストデータ作成
+		int cnt = scheduleDao.insertSchedule(10, "毛利さんにメールする", new Timestamp(new Date().getTime()), false );
+
+		assertEquals(1, cnt);
+	}
+
+	@Test
+	public void テスト57() throws Exception {
+
+		// テストデータ作成
+		int cnt = scheduleDao.insertSchedule(10, null, null, true );
+
+		assertEquals(1, cnt);
+	}
+
+	@Test
+	public void テスト58() throws Exception {
+
+		// テストデータ作成
+		int cnt = scheduleDao.insertSchedule(null, null, null, true );
+
+		assertEquals(0, cnt);
+	}
+
+	@Test
+	public void テスト59() throws Exception {
+
+		// テストデータ作成
+		int cnt = scheduleDao.insertSchedule(1, "12345678902234567890323456789042345678905234567890623456789072345678908234567890923456789012345678902343これで128", null, true );
+
+		assertEquals(1, cnt);
+	}
+
+	@Test
+	public void テスト60() throws Exception {
+
+		// テストデータ作成
+		int cnt = scheduleDao.insertSchedule(1, "1234567890223456789032345678904234567890523456789062345678907234567890823456789092345678901234567890234これで129", null, true );
+
+		assertEquals(0, cnt);
+	}
+
+	@Test
+	public void テスト61() throws Exception {
+
+		// テストデータ作成
+		int cnt = scheduleDao.insertSchedule(99, "毛利さんにメールする", null, false );
+
+		assertEquals(0, cnt);
 	}
 
 }
