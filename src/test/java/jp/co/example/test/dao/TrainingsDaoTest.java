762a028a7ca9b7a50b2a642a22b6e6fc054cf7e2
takahashi <2017se_r.takahashi@axiz.co.jp>
2017-06-08 16:03:10 +0900

    no message

--- src/test/java/jp/co/example/test/dao/TrainingsDaoTest.java
+++ src/test/java/jp/co/example/test/dao/TrainingsDaoTest.java
@@ -2,6 +2,8 @@ package jp.co.example.test.dao;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.*;
 import org.junit.runner.*;
 import org.springframework.beans.factory.annotation.*;
@@ -9,6 +11,7 @@ import org.springframework.boot.test.context.*;
 import org.springframework.test.context.junit4.*;
 
 import jp.co.example.dao.*;
+import jp.co.example.entity.Users;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,13 +24,38 @@ public class TrainingsDaoTest {
 	@Autowired
 	private TrainingsDao trainingDao;
 
+//	@Test
+//	public void メールが取得できる() throws Exception {
+//		assertTrue(true);
+//	}
+//
+//	@Test
+//	public void メールが取得できない() throws Exception {
+//		assertTrue(true);
+//	}
+
 	@Test
-	public void メールが取得できる() throws Exception{
-		assertTrue(true);
+
+	public void テスト69() throws Exception {
+		Users user = trainingDao.getTrainingId(2);
+
+		assertEquals("1", user.getUserId());
+		assertEquals(Integer.valueOf(1), user.getCompanyId());
+		assertEquals(Integer.valueOf(0), user.getAuthority());
+	}
+
+	public void テスト70() throws Exception {
+
+		int
+		assertTrue(false);
+
 	}
 
 	@Test
-	public void メールが取得できない() throws Exception{
-		assertTrue(true);
+	public void テスト71() throws Exception {
+		List list = trainingsDao. AllRooms();
+
+		assertEquals();
 	}
+
 }
