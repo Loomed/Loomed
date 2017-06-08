114e78ed262946bbf34c7e6fa9511f56a678f181
mkt-S <fr0m.ma.t0.s@gmail.com>
2017-06-08 19:50:29 +0900

    no message

--- src/test/java/jp/co/example/test/dao/SchedulesDaoTest.java
+++ src/test/java/jp/co/example/test/dao/SchedulesDaoTest.java
@@ -2,21 +2,22 @@ package jp.co.example.test.dao;
 
 import static org.junit.Assert.*;
 
-import java.sql.*;
-import java.util.*;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
-import org.junit.*;
-import org.junit.runner.*;
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.boot.test.context.*;
-import org.springframework.test.context.*;
-import org.springframework.test.context.junit4.*;
-import org.springframework.transaction.annotation.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
-import jp.co.example.*;
-import jp.co.example.dao.*;
-import jp.co.example.entity.*;
+import jp.co.example.LoomedApplication;
+import jp.co.example.dao.SchedulesDao;
+import jp.co.example.entity.Schedules;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -30,18 +31,24 @@ public class SchedulesDaoTest {
 	public void テスト45() throws Exception {
 
 		// テストデータ作成
-		Schedules schedule = scheduleDao.selectScheduleWhereUserIdAndDate();
+		final SimpleDateFormat SDF_DATETIME = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+		List<Schedules> schedule = scheduleDao.selectScheduleWhereUserIdAndDate(1,
+				new Timestamp(SDF_DATETIME.parse("2017/6/6 0:0:0").getTime()),
+				new Timestamp(SDF_DATETIME.parse("2017/6/7 0:0:0").getTime()));
 
-		assertTrue(false);
+		assertEquals(3, schedule.size());
 	}
 
 	@Test
 	public void テスト46() throws Exception {
 
 		// テストデータ作成
-		Schedules schedule = scheduleDao.selectScheduleWhereUserIdAndDate(userId, dateMin, dateMax);
+		final SimpleDateFormat SDF_DATETIME = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+		List<Schedules> schedule = scheduleDao.selectScheduleWhereUserIdAndDate(1,
+				new Timestamp(SDF_DATETIME.parse("2016/1/1 0:0:0").getTime()),
+				new Timestamp(SDF_DATETIME.parse("2016/1/2 0:0:0").getTime()));
 
-		assertTrue(false);
+		assertEquals(0, schedule.size());
 	}
 
 	@Test
@@ -50,17 +57,17 @@ public class SchedulesDaoTest {
 		// テストデータ作成
 		List<Schedules> schedule = scheduleDao.getInpoSche();
 
-		assertTrue(false);
+		assertEquals(4, schedule.size());
 	}
 
-	@Test
-	public void テスト48() throws Exception {
-
-		// テストデータ作成
-		List<Schedules> schedule = scheduleDao.getInpoSche();
-
-		assertTrue(false);
-	}
+//	@Test
+//	public void テスト48() throws Exception {
+//
+//		// テストデータ作成
+//		List<Schedules> schedule = scheduleDao.getInpoSche();
+//
+//		assertTrue(false);
+//	}
 
 	@Test
 	public void テスト49() throws Exception {
@@ -129,7 +136,7 @@ public class SchedulesDaoTest {
 	public void テスト56() throws Exception {
 
 		// テストデータ作成
-		int cnt = scheduleDao.insertSchedule(10, "毛利さんにメールする", new Timestamp(new Date().getTime()), false );
+		int cnt = scheduleDao.insertSchedule(10, "毛利さんにメールする", new Timestamp(new Date().getTime()), false);
 
 		assertEquals(1, cnt);
 	}
@@ -138,7 +145,7 @@ public class SchedulesDaoTest {
 	public void テスト57() throws Exception {
 
 		// テストデータ作成
-		int cnt = scheduleDao.insertSchedule(10, null, null, true );
+		int cnt = scheduleDao.insertSchedule(10, null, null, true);
 
 		assertEquals(1, cnt);
 	}
@@ -147,7 +154,7 @@ public class SchedulesDaoTest {
 	public void テスト58() throws Exception {
 
 		// テストデータ作成
-		int cnt = scheduleDao.insertSchedule(null, null, null, true );
+		int cnt = scheduleDao.insertSchedule(null, null, null, true);
 
 		assertEquals(0, cnt);
 	}
@@ -156,7 +163,9 @@ public class SchedulesDaoTest {
 	public void テスト59() throws Exception {
 
 		// テストデータ作成
-		int cnt = scheduleDao.insertSchedule(1, "12345678902234567890323456789042345678905234567890623456789072345678908234567890923456789012345678902343これで128", null, true );
+		int cnt = scheduleDao.insertSchedule(1,
+				"12345678902234567890323456789042345678905234567890623456789072345678908234567890923456789012345678902343これで128",
+				null, true);
 
 		assertEquals(1, cnt);
 	}
@@ -165,7 +174,9 @@ public class SchedulesDaoTest {
 	public void テスト60() throws Exception {
 
 		// テストデータ作成
-		int cnt = scheduleDao.insertSchedule(1, "1234567890223456789032345678904234567890523456789062345678907234567890823456789092345678901234567890234これで129", null, true );
+		int cnt = scheduleDao.insertSchedule(1,
+				"1234567890223456789032345678904234567890523456789062345678907234567890823456789092345678901234567890234これで129",
+				null, true);
 
 		assertEquals(0, cnt);
 	}
@@ -174,7 +185,7 @@ public class SchedulesDaoTest {
 	public void テスト61() throws Exception {
 
 		// テストデータ作成
-		int cnt = scheduleDao.insertSchedule(99, "毛利さんにメールする", null, false );
+		int cnt = scheduleDao.insertSchedule(99, "毛利さんにメールする", null, false);
 
 		assertEquals(0, cnt);
 	}
