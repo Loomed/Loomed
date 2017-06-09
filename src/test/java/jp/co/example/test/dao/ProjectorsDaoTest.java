c30967a9d0e9ac2cac68653f7044cc7701eced50
mkt-S <fr0m.ma.t0.s@gmail.com>
2017-06-09 11:09:47 +0900

    DaoTest

--- src/test/java/jp/co/example/test/dao/ProjectorsDaoTest.java
+++ src/test/java/jp/co/example/test/dao/ProjectorsDaoTest.java
@@ -2,17 +2,22 @@ package jp.co.example.test.dao;
 
 import static org.junit.Assert.*;
 
-import org.junit.*;
-import org.junit.runner.*;
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.boot.test.context.*;
-import org.springframework.test.context.*;
-import org.springframework.test.context.junit4.*;
-import org.springframework.transaction.annotation.*;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.List;
 
-import jp.co.example.*;
-import jp.co.example.dao.*;
-import jp.co.example.entity.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.example.LoomedApplication;
+import jp.co.example.dao.ProjectorsDao;
+import jp.co.example.entity.Projectors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -27,109 +32,131 @@ public class ProjectorsDaoTest {
 	public void テスト34() throws Exception {
 
 		// テストデータ作成
-		Projectors pro = projectorsDao.selectProjectorsWhereTrainingIdAndDate(trainingId, dateMin, dateMax);
+		int trainingId = 2;
+		final SimpleDateFormat SDF_DATETIME = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+		Timestamp dateMin = new Timestamp(SDF_DATETIME.parse("2017/6/5 0:0:0").getTime());
+		Timestamp dateMax = new Timestamp(SDF_DATETIME.parse("2017/6/5 11:35:0").getTime());
+		List<Projectors> pro = projectorsDao.selectProjectorsWhereTrainingIdAndDate(trainingId, dateMin, dateMax);
 
-		assertTrue(false);
+		assertEquals(3, pro.size());
 	}
 
 	@Test
 	public void テスト35() throws Exception {
 
 		// テストデータ作成
-		Projectors pro = projectorsDao.selectProjectorsWhereTrainingIdAndDate(trainingId, dateMin, dateMax);
+		int trainingId = 1;
+		final SimpleDateFormat SDF_DATETIME = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+		Timestamp dateMin = new Timestamp(SDF_DATETIME.parse("2017/6/5 0:0:0").getTime());
+		Timestamp dateMax = new Timestamp(SDF_DATETIME.parse("2017/6/5 11:35:0").getTime());
+		List<Projectors> pro = projectorsDao.selectProjectorsWhereTrainingIdAndDate(trainingId, dateMin, dateMax);
 
-		assertTrue(false);
+		assertEquals(0, pro.size());
 	}
 
 	@Test
 	public void テスト36() throws Exception {
 
 		// テストデータ作成
-		Projectors pro = projectorsDao.selectProjectorsWhereTrainingIdAndDateTime(trainingId, dateMin, dateMax);
+		int trainingId = 2;
+		final SimpleDateFormat SDF_DATETIME = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+		Timestamp dateMin = new Timestamp(SDF_DATETIME.parse("2017/6/5 11:0:0").getTime());
+		List<Projectors> pro = projectorsDao.selectProjectorsWhereTrainingIdAndDateTime(trainingId, dateMin);
 
-		assertTrue(false);
+		assertEquals(0, pro.size());
 	}
 
 	@Test
 	public void テスト37() throws Exception {
 
 		// テストデータ作成
-		Projectors pro = projectorsDao.selectProjectorsWhereTrainingIdAndDateTime(trainingId, dateMin, dateMax);
+		int trainingId = 2;
+		final SimpleDateFormat SDF_DATETIME = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+		Timestamp dateMin = new Timestamp(SDF_DATETIME.parse("2017/6/5 9:0:0").getTime());
+		List<Projectors> pro = projectorsDao.selectProjectorsWhereTrainingIdAndDateTime(trainingId, dateMin);
 
-		assertTrue(false);
+		assertEquals(2, pro.size());
 	}
 
 	@Test
 	public void テスト38() throws Exception {
 
 		// テストデータ作成
-		int cnt = projectorsDao.deleteWhereProjectorId(projectorId)
+		int projectorId = 2;
+		int cnt = projectorsDao.deleteWhereProjectorId(projectorId);
+		System.out.println("tes:" + cnt);
 
-		assertTrue(false);
+		assertEquals(1,cnt);
 	}
 
-	@Test
-	public void テスト39() throws Exception {
-
-		// テストデータ作成
-		int cnt = projectorsDao.deleteWhereProjectorId(projectorId)
-
-		assertTrue(false);
-	}
+//	@Test
+//	public void テスト39() throws Exception {
+//
+//		// テストデータ作成
+//		int cnt = projectorsDao.deleteWhereProjectorId(projectorId)
+//
+//		assertTrue(false);
+//	}
 
 	@Test
 	public void テスト40() throws Exception {
 
 		// テストデータ作成
-		int cnt = projectorsDao.insertProjectoReserve(trainingId, projectorNumber, userId, reserveTime))
+		int trainingId = 3;
+		int projectorNumber = 1;
+		int userId = 10;
+		final SimpleDateFormat SDF_DATETIME = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+		Timestamp reserveTime = new Timestamp(SDF_DATETIME.parse("2017/6/5 11:0:0").getTime());
 
-		assertTrue(false);
+		int cnt = projectorsDao.insertProjectoReserve(trainingId, projectorNumber, userId, reserveTime);
+
+		assertEquals(1,cnt);
 	}
 
-	@Test
+	@Test(expected=DataAccessException.class)
 	public void テスト41() throws Exception {
 
 		// テストデータ作成
-		int cnt = projectorsDao.insertProjectoReserve(null, null, null, null);
+		projectorsDao.insertProjectoReserve(null, null, null, null);
 
-		assertTrue(false);
 	}
 
-	@Test
-	public void テスト42() throws Exception {
+//	@Test
+//	public void テスト42() throws Exception {
+//
+//		// テストデータ作成
+//		int cnt = projectorsDao.insertProjectoReserve(null, null, null, null);
+//
+//		assertTrue(false);
+//	}
+//
+//	@Test
+//	public void テスト43() throws Exception {
+//
+//		// テストデータ作成
+//		int cnt = projectorsDao.insertProjectoReserve(null, null, null, null);
+//
+//		assertTrue(false);
+//	}
 
-		// テストデータ作成
-		int cnt = projectorsDao.insertProjectoReserve(null, null, null, null);
-
-		assertTrue(false);
-	}
-
-	@Test
+	@Test(expected=DataAccessException.class)
 	public void テスト43() throws Exception {
 
 		// テストデータ作成
-		int cnt = projectorsDao.insertProjectoReserve(null, null, null, null);
+		final SimpleDateFormat SDF_DATETIME = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+		Timestamp reserveTime = new Timestamp(SDF_DATETIME.parse("2017/6/5 11:0:0").getTime());
 
-		assertTrue(false);
+		projectorsDao.insertProjectoReserve(null, 2, 2, reserveTime);
+
 	}
 
-	@Test
-	public void テスト44() throws Exception {
-
-		// テストデータ作成
-		int cnt = projectorsDao.insertProjectoReserve(null, null, null, null);
-
-		assertTrue(false);
-	}
-
-	@Test
-	public void テスト45() throws Exception {
-
-		// テストデータ作成
-		int cnt = projectorsDao.insertProjectoReserve(null, null, null, null);
-
-		assertTrue(false);
-	}
-
+//	@Test
+//	public void テスト44() throws Exception {
+//
+//		// テストデータ作成
+//		int cnt = projectorsDao.insertProjectoReserve(null, null, null, null);
+//
+//		assertTrue(false);
+//	}
 
 }
